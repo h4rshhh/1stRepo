9ec8b96ce362818f8bafb860cc4df33f935745b3
Harsh Kumar <113852094+h4rshhh@users.noreply.github.com>
2023-05-22 22:36:37 +0530

    Update tripletsum.cpp

--- tripletsum.cpp
+++ tripletsum.cpp
@@ -4,9 +4,7 @@ int tripletSum(int *input, int size, int x){
 		int temp1 = input[i];
 
 		for(int j = i+1;j<size;j++){
-			int temp2 = input[j];
-			if(i == j){
-				continue;
+			int temp2 = input[j];			
 			}
 			for(int k = j+1;k<size;k++){				
 				if(temp1 + temp2 + input[k]==x){
