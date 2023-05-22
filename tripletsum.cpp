08f43e21688c71a61b8bbb8e411c7172406c3efa
Harsh Kumar <113852094+h4rshhh@users.noreply.github.com>
2023-05-22 22:28:03 +0530

    Create tripletsum.cpp
    You have been given a random integer array/list(ARR) and a number X. Find and return the number of triplets in the array/list which sum to X.

---
+++ tripletsum.cpp
@@ -0,0 +1,20 @@
+int tripletSum(int *input, int size, int x){
+	int count = 0;
+	for(int i = 0;i<size;i++){
+		int temp1 = input[i];
+
+		for(int j = i+1;j<size;j++){
+			int temp2 = input[j];
+			if(i == j){
+				continue;
+			}
+			for(int k = j+1;k<size;k++){				
+				if(temp1 + temp2 + input[k]==x){
+					count++;					
+				}
+			}
+		}		
+	}
+	return count;
+
+}
