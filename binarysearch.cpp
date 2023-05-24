a2e5fa88964645b4b3d1014f0c81249faa13693b
Harsh Kumar <113852094+h4rshhh@users.noreply.github.com>
2023-05-24 23:50:58 +0530

    Create binarysearch.cpp
    Binary search to find the target integer in an array.

---
+++ binarysearch.cpp
@@ -0,0 +1,23 @@
+int search(vector<int>& nums, int target){
+    int n = nums.size();
+
+    int low = 0;
+    int high = n-1;
+
+
+    while(high>=low){
+        int mid = (low + high)/2;
+
+        if(target == nums[mid]){
+            return mid;
+        }
+        else if(target > nums[mid]){
+            low = mid+1;
+        }
+        else if(target < nums[mid]){
+            high = mid-1;
+        }
+
+    }
+    return -1;
+}
