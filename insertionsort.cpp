0a3b29132e9a1ba7c284772335c16f984fe34e4a
Harsh Kumar <113852094+h4rshhh@users.noreply.github.com>
2023-05-28 16:20:21 +0530

    Update insertionsort.cpp

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 void insertionSort(int input[], int size){
     int i,j,key;
     for(i=1;i<size;i++){
@@ -9,4 +12,24 @@ void insertionSort(int input[], int size){
         }
         input[j+1] = key;
     }
+}
+
+int main(){
+    int size;
+    cout<<"Enter the size of the array: "<<endl;
+    cin>>size;
+    int input[size];
+    cout<<"Enter the elements of the array: "<<endl;
+    
+    for(int i = 0;i<size;i++){
+        cin>>input[i];
+    }
+    insertionSort(input,size);
+    
+    cout<<"Sorted Array: "<<endl;
+    for(int k = 0;k<size;k++){
+        cout<<input[k]<<" ";
+    }
+    
+    return 0;
 }
